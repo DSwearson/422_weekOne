8360bf9bcb8c84f4a7af4dc1d155c7c0dbcd9242
Daniel J. Swearson <dswearson60@gmail.com>
2023-05-11 19:20:49 -0500

    Release 2 Search by Name & Age

--- src/PetTester.java
+++ src/PetTester.java
@@ -19,6 +19,12 @@ public class PetTester {
 				viewAllPets(pets);
 			} else if(choice == 2) {
 			addPets(input, pets);	
+			} else if(choice == 5) {
+				List<Pet> results = searchByName(input, pets);
+				viewAllPets(results);
+			} else if(choice == 6) {
+				List<Pet> results = searchByAge(input, pets);
+				viewAllPets(results);
 			}
 			choice = showMenu(input);
 		}
@@ -30,6 +36,8 @@ public class PetTester {
 		System.out.println("What would you like to do?");
 		System.out.println("1) View all pets");
 		System.out.println("2) Add more pets");
+		System.out.println("5) Search pets by name");
+		System.out.println("6) Search pets by age");
 		System.out.println("7) Exit program");
 		System.out.print("Your choice:");
 		int choice = input.nextInt();
@@ -66,5 +74,30 @@ public class PetTester {
 			Pet pet = new Pet(name, age);
 			pets.add(pet);
 		}
+	}
+	
+	private static List<Pet> searchByName(Scanner input, List<Pet> pets) {
+		List<Pet> results = new ArrayList<>();
+		System.out.print("Enter a name to search:");
+		String name = input.nextLine();
+		for(Pet pet: pets) {
+			if(pet.getName().equalsIgnoreCase(name)) {
+				results.add(pet);
+			}
+		}
+		return results;
+	}
+	
+	private static List<Pet> searchByAge(Scanner input, List<Pet> pets) {
+		List<Pet> results = new ArrayList<>();
+		System.out.print("Enter age to search:");
+		int age = input.nextInt();
+		input.nextLine();
+		for(Pet pet: pets) {
+			if(pet.getAge() == age) {
+				results.add(pet);
+			}
+		}
+		return results;
 	}
 }
